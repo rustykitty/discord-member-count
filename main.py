d1e524c0d5930ef4e9b3f9731dc723e4d4001a7f
Rusty <rusty3925@gmail.com>
2026-04-25 13:25:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from discord-member-count!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ retrieve.py
@@ -0,0 +1,37 @@
+import requests
+
+import logging
+
+logger = logging.Logger(__name__)
+
+# get config
+
+import json
+with open("config.json") as fp:
+    CONFIG = json.load(fp)
+
+# https://www.reddit.com/r/Discord_Bots/comments/ugp938/api_is_it_possible_to_fetch_the_member_count_with/
+API_ENDPOINT_BASE = "https://discord.com/api/v9/invites/"
+
+API_ENDPOINT = API_ENDPOINT_BASE + CONFIG["invite"]
+
+# https://stackoverflow.com/a/28147286
+import datetime
+now = datetime.datetime.now().isoformat()
+
+print("Currently", now)
+
+response = requests.get(API_ENDPOINT)
+
+if response.status_code == 200:
+    data = response.json()
+    count = data["profile"]["member_count"]
+    date = datetime.date.today()
+    time = datetime.datetime.now().time()
+    with open("member_count.out", "a") as fp:
+        # date, time, member count
+        fp.write(f"{date}\t{time}\t{count}\n")
+else:
+    print("Non-200 status code returned. Please check it.")
+    print("HTTP", response.status_code)
+    print(response.text)
