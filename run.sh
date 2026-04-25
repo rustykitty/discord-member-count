bb0811e7731f9c32abe188f691067458bb7265aa
Rusty <rusty3925@gmail.com>
2026-04-25 15:13:19 -0700

    add err log

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
-uv sync
-uv run retrieve.py
+set -e
 
+# https://stackoverflow.com/a/49671326
+function nodupe() {
+    if [[ -e "$1" ]]
+    then
+        echo "$(printf "%s\n" "${1}"* | wc -l)"
+    else
+        echo "$1"
+    fi
+}
+
+cd "$(dirname "$0")"
+
+LOGDIR="./log"
+
+time=$(date +"%Y-%m-%d_%H:%M:%S")
+errlog="$LOGDIR/stderr_$time.log"
+errlog="$(nodupe "$errlog")"
+
+mkdir -p log
+
+uv sync
+uv run retrieve.py 2> "$errlog"
